b37af52160df58fb614802a19a9ed27a7ab9681b
Mariam215 <164162868+Mariam215@users.noreply.github.com>
2025-01-21 16:42:18 +0200

    add  pagetitle in navbar component

--- src/app/global.service.ts
+++ src/app/global.service.ts
@@ -5,7 +5,9 @@ import { Injectable } from '@angular/core';
 })
 export class GlobalService {
   logoPage:string="62d3960a755658580bdded8a_logo.png"
+  imgSection:string="62e0bd1e0f555f392e090236_breadcrumb-image-1-p-800.png"
   constructor() { 
-    this.logoPage=this.logoPage
+    this.logoPage=this.logoPage;
+    this.imgSection=this.imgSection;
   }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { GlobalService } from '../global.service';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 @Component({
   selector: 'app-navbar',
   standalone: false,
-  
+
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
-  logoPage:string="62d3960a755658580bdded8a_logo.png"
+export class NavbarComponent implements OnInit {
+  logoPage: string = "62d3960a755658580bdded8a_logo.png";
+  image: string = '';
+  pageTitle: string = '';
+  constructor(private _GlobalServices: GlobalService, private router: Router, private activatedRoute: ActivatedRoute) {
+
+  }
+  ngOnInit(): void {
+    this.image = this._GlobalServices.imgSection;
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd), // للتأكد من أن الحدث هو نهاية التنقل
+        map(() => {
+          let child = this.activatedRoute.firstChild;
+          while (child) {
+            if (child.snapshot.data['title']) {
+              return child.snapshot.data['title'];
+            }
+            child = child.firstChild;
+          }
+          return 'Default Title'; // عنوان افتراضي إذا لم يكن هناك عنوان
+        })
+      )
+      .subscribe(title => {
+        this.pageTitle = title;
+      });
+  }
 }
+
