7b52d2b988441287c6fdbefbe8e0666c9ca296c0
Mariam215 <164162868+Mariam215@users.noreply.github.com>
2025-01-28 14:00:29 +0200

    create login component

--- src/app/Account/login/login.component.ts
+++ src/app/Account/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: false,
@@ -9,4 +12,25 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 title="Login"
+formLogin: FormGroup = new FormGroup({
+  email: new FormControl('', [Validators.required, Validators.email]),
+  password: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{6}$')]), // كلمة مرور مكونة من 6 أرقام فقط
+})
+constructor(private _AuthService: AuthService,private toastr: ToastrService,private router:Router){}
+ handle(form:FormGroup){
+  if(form.valid){
+    this._AuthService.Login(form.value).subscribe({
+       next:(res)=>{
+         console.log(res)
+         this.toastr.success("Login Successsfully")
+         this.router.navigate(['/'])
+       },
+       error:(err)=>{
+        console.log(err)
+        this.toastr.error(err.error.message);
+        
+       }
+    })
+  }
+ }
 }
