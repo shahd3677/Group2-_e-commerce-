3ae64628e0dbaa96682f75c776e789bb4d2610cc
Mariam215 <164162868+Mariam215@users.noreply.github.com>
2025-04-28 23:53:56 +0300

    create auth and user endpoint

---
+++ backEnd/routers/auth_route.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { forgetPassword, login, resetPassword, sendEmail, signUP } = require("../controllers/auth.js");
+
+
+const authRouter = express.Router();
+
+authRouter.post("/signUP", signUP);
+authRouter.post("/login", login);
+authRouter.post("/sendEmail", sendEmail);
+authRouter.post("/forgetPassword", forgetPassword);
+authRouter.post("/reset", resetPassword);
+
+module.exports = authRouter;
---
+++ backEnd/utilits/sendEmail.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer");
+
+const sendEmailFunction = async ({ to, subject, text }) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    secure: true,
+    port: 465,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL.PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    text,
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+};
+
+module.exports = { sendEmailFunction };
